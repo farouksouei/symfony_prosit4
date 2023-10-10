54f952a7540c5ab9ba8ac63a7f9eaf84c0376fe6
Farouk Souei <farouksouei@gmail.com>
2023-10-10 11:22:48 +0100

    first commit

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/author')]
+class AuthorController extends AbstractController
+{
+    #[Route('/', name: 'app_author_index', methods: ['GET'])]
+    public function index(AuthorRepository $authorRepository): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'authors' => $authorRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_author_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $author = new Author();
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($author);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_author_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('author/new.html.twig', [
+            'author' => $author,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_author_show', methods: ['GET'])]
+    public function show(Author $author): Response
+    {
+        return $this->render('author/show.html.twig', [
+            'author' => $author,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_author_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Author $author, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_author_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('author/edit.html.twig', [
+            'author' => $author,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_author_delete', methods: ['POST'])]
+    public function delete(Request $request, Author $author, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$author->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($author);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_author_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
